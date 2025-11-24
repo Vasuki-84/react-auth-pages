ad09a210ef8d07780dae7689373aff782d763ae4
Vasuki t <iamvasukithangaraj@gmail.com>
2025-11-24 12:43:23 +0530

    added login page

--- react-auth-pages/src/Components/Login.jsx
+++ react-auth-pages/src/Components/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const loginUser = async (e) => {
+    e.preventDefault();
+    try {
+      const userData = { email, password };
+      await axios.post(`http://localhost:5000/login`, userData);
+      alert("login successfull");
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <h2>Login Page</h2>
+      <form onSubmit={loginUser}>
+        <label>Email</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Password</label>
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- react-auth-pages/src/Components/Signup.jsx
+++ react-auth-pages/src/Components/Signup.jsx
@@ -1,18 +1,23 @@
 import axios from "axios";
-import React, {  useState } from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function Signup() {
-    const [id,setId]= useState("");
+  const [id, setId] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const createUser = async (e) => {
     e.preventDefault();
     try {
       const userData = { name, email, password };
+
       await axios.post(`http://localhost:5000/signup`, userData);
       alert("Signup successfull");
+      navigate("/login")
     } catch (err) {
       console.log(err.message);
     }
@@ -23,11 +28,7 @@ function Signup() {
       <h2 className="signup-title">SignUp form</h2>
       <form className="signup-form" onSubmit={createUser}>
         <label>Id</label>
-        <input
-          type="text"
-          value={id}
-          onChange={(e) => setId(e.target.value)}
-        />
+        <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
         <label>Name</label>
         <input
           type="text"
@@ -46,7 +47,7 @@ function Signup() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="signup-btn" type="submit" >
+        <button className="signup-btn" type="submit">
           Submit
         </button>
       </form>
