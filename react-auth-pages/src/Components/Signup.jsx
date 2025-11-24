404da8ef3de33afc76810fe8d808cbd975823705
Vasuki t <iamvasukithangaraj@gmail.com>
2025-11-24 12:27:04 +0530

    added signup page

--- react-auth-pages/src/Components/Signup.jsx
+++ react-auth-pages/src/Components/Signup.jsx
@@ -1,16 +1,54 @@
-import React from "react";
+import axios from "axios";
+import React, {  useState } from "react";
 
 function Signup() {
+    const [id,setId]= useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const createUser = async (e) => {
+    e.preventDefault();
+    try {
+      const userData = { name, email, password };
+      await axios.post(`http://localhost:5000/signup`, userData);
+      alert("Signup successfull");
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
     <div className="signup-container">
       <h2 className="signup-title">SignUp form</h2>
-      <form className="signup-form">
+      <form className="signup-form" onSubmit={createUser}>
+        <label>Id</label>
+        <input
+          type="text"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
         <label>Name</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <label>Email</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label>Password</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="signup-btn" type="submit" >
+          Submit
+        </button>
       </form>
     </div>
   );
